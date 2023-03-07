1dc9aeb8ee79aab851bf982049311c72ac3190d0
keni7689 <103092905+keni7689@users.noreply.github.com>
2023-03-07 10:20:02 +0530

    commit1

---
+++ home.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css?.v=<?php echo time();?>">
+</head>
+
+<body>
+    <!-- header section start -->
+    <section class="header">
+        <a href="home.php" class="logo">travel_Website.</a>
+        <nav class="navbar">
+            <a href="home.php" class="Noidea">Home</a>
+            <a href="about.php" class="Noidea">About</a>
+            <a href="package.php" class="Noidea">Pakage</a>
+            <a href="book.php" class="Noidea">Book</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+
+    
+
+
+    <!-- header section end -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="script.js?.v=<?php echo time();?>" ></script>
+    
+</body>
+
+</html>
